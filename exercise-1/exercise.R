55cb93f1c0b8be409adb5e5afcad58fd177dacee
Matthew Mcvicker <23mcvicker@gmail.com>
2018-04-20 13:25:48 -0700

    Participation

--- exercise-1/exercise.R
+++ exercise-1/exercise.R
@@ -8,12 +8,13 @@ library(dplyr)
 
 # Create a variable for the API's base URI (https://api.github.com)
 
-
+base_uri <- "https://api.github.com"
 # Under the "Repositories" category of the API, 
 # find the endpoint that will list repos in an organization. Then, 
 # Create a variable `resource` that stores the endpoint for the "info201"
 # organization repos (this is the PATH to the resource of interest).
 # (FYI: this is where we keep the book code and master exercise sets!)
+resource <- "/orgs/info201/repos"
 
 
 # Send a GET request to this endpoint (the `base_uri`` followed by `resource`)
